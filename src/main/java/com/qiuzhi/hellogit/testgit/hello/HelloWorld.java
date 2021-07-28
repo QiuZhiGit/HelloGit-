29354bd9e7571bb20d5ec72587713a35a71e4b41
qiuzhi <2714666146@qq.com>
2021-07-28 15:46:07 +0800

    添加文件

---
+++ src/main/java/com/qiuzhi/hellogit/testgit/hello/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.qiuzhi.hellogit.testgit.hello;/**
+ * @Author: qiuzhi
+ * @Date: 2021/7/28 15:16
+ */
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @description: TODO
+ * @author qiuzhi
+ * @date 2021/7/28 15:16
+ * @version 1.0
+ */
+@Component
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("更新1");
+    }
+}
