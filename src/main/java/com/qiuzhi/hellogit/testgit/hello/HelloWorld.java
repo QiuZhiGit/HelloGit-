6021426f72ea28202bd097f29a8420a40020df15
qiuzhi <2714666146@qq.com>
2021-07-28 16:03:07 +0800

    创建分支

--- src/main/java/com/qiuzhi/hellogit/testgit/hello/HelloWorld.java
+++ src/main/java/com/qiuzhi/hellogit/testgit/hello/HelloWorld.java
@@ -17,5 +17,7 @@ public class HelloWorld {
         System.out.println("hello world!");
         System.out.println("更新1");
         System.out.println("更新2");
+        System.out.println("创建分支");
+
     }
 }
